94ccbf9d069b41d6d488fdec5c7521e60718cf15
danilolimaext <danilo_jose_lima@hotmail.com>
2023-06-30 16:53:13 -0300

    Estrutura do site

---
+++ js/tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        'blue-light': '#08BAEA',
+        'blue-dark': '#0F357D',
+        'gray-light': '#9EB3C2',
+        'gray-dark': '#42577D',
+        'white': '#ffffff'
+      }
+    },
+    fontFamily: {
+      'sans': ['Be Vietnam Pro', 'sans-serif']
+    }
+  }
+}
